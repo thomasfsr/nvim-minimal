c79ed20aff489ef80fe97929e1671bbeb1308e3d
Thomás Freire <thomas.fsr@gmail.com>
2026-03-04 23:40:18 -0300

    keymap e in nvim-tree to open new buffer

--- init.lua
+++ init.lua
@@ -516,7 +516,20 @@ end
 
 setup_treesitter()
 
+local function my_on_attach(bufnr)
+    local api = require("nvim-tree.api")
+
+    local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    api.config.mappings.default_on_attach(bufnr)
+
+    vim.keymap.set("n", "e", api.node.open.edit, opts("Open"))
+end
+
 require("nvim-tree").setup({
+on_attach = my_on_attach,
 	view = {
 		width = 35,
 	},
